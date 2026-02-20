2e0905ad9a66f0df7ee7ee90be86a08d280b92e2
Shivangi04671 <shivangi.s2242@gmail.com>
2026-02-20 15:57:18 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,23 +1,54 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review= require("./review.js");
+const User= require("./user.js");
 
-const listingSchema = new Schema ({
-    title:  {
+const listingSchema = new Schema({
+  title: {
     type: String,
     required: true,
+  },
+  description: String,
+  image: {
+    filename:{
+        type: String,
+        default: "listingimage"
     },
-    description: String,
-    image: {
-    type: String,
-    default:
-     "https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set: (v) => (v ==="") ? "https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-    : v,
+    url:{
+        type: String,
+        default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      //for when image is given, but image link is not provided  
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
     },
-    price: Number,
-    location: String,
-    country: String,
+    // type: String,
+    //for image not existing or null
+    
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
+listingSchema.post("findOneAndDelete", async(listing) => {
+  if(listing){
+    await Review.deleteMany({_id: {$in: listing.reviews} });
+  }
+});
+
+//Exporting in the form of model
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
