f77eb12c0e83c788069275efbdea0a28ff1c03ce
Shivangi04671 <shivangi.s2242@gmail.com>
2026-02-20 15:58:15 +0530

    index.js

--- init/index.js
+++ init/index.js
@@ -25,6 +25,7 @@ async function main() {
 
 const initDB = async() => {
     await   Listing.deleteMany({});
+    initData.data= initData.data.map((obj) => ({...obj, owner: "6971d969f90f4678369a3059",}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
